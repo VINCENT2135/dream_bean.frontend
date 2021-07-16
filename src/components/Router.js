db688f7fdafe190fd5c1825686aaf7755fb3f12e
Vincent Ivos <vincentivos1@gmail.com>
2021-07-16 15:01:09 -0400

    Router Component

---
+++ src/components/Router.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Switch, Route } from "react-router-dom"
+
+
+
+const Router = () => {
+    return (
+    <div>
+        <Switch> 
+            
+        </Switch>
+
+    </div>
+    );
+};
+
+export default Router;
