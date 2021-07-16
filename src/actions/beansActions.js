df0503286ef3679e9ae608e89e43b9c2a33efa0e
Vincent Ivos <vincentivos1@gmail.com>
2021-07-15 21:42:45 -0400

    AddBeans/Create BeansForm Component

--- src/actions/beansActions.js
+++ src/actions/beansActions.js
@@ -10,3 +10,25 @@ export const fetchBeans = () => {   console.log("Hello There");
         .then( beans => dispatch({type: 'FETCH_BEANS', payload: beans})) 
     }
 }
+
+
+
+
+export const addBeans = beans => { console.log("Hello There");
+    return dispatch  => {
+        fetch('http://127.0.0.1:3000/beans', {
+             
+                method: 'POST',
+                body: JSON.stringify(beans), 
+                headers: { 'Content-Type': 'application/json',
+                'Accept': 'application/json'}
+              
+            })
+        .then(resp => resp.json())
+        .then( beans => dispatch( {type: 'ADD_BEANS', payload: beans}))
+    }
+} 
+
+
+
+
