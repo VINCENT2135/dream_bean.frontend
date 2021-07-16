c62e31f60eeefd9cfbf2ad5fb0d63f4618a0dd0e
Vincent Ivos <vincentivos1@gmail.com>
2021-07-16 12:12:13 -0400

    BeansForm.js

---
+++ src/components/BeansForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+
+ class BeansForm extends Component {
+
+state = {
+    name: '',
+    origin: ''
+
+}
+  
+    render() {
+        return (
+            <form>
+                
+              
+           
+            </form> 
+           
+        )
+    }
+}
+
+export default BeansForm  
