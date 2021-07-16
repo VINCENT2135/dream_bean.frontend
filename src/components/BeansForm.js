d2d02c15259fdeda401df9957414b35c3394da58
Vincent Ivos <vincentivos1@gmail.com>
2021-07-16 12:23:42 -0400

    html

--- src/components/BeansForm.js
+++ src/components/BeansForm.js
@@ -13,7 +13,12 @@ state = {
     render() {
         return (
             <form>
-                
+                <label> Name: </label>
+                <br/>
+                <input type="text" value={this.state.title} onChange={this.handleChange} name="name"/>
+                <br/>
+                <label> Origin: </label>
+             
               
            
             </form> 
