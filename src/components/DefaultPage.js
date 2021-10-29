48b79fdcee73d84b59212606b423f31dec79f56f
Vincent Ivos <vincentivos1@gmail.com>
2021-10-28 22:17:11 -0400

    Refactor nav component

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import img9 from '../images/arabic-coffee.jpeg'
 
-class Nav extends Component{ 
-    render() {
-        return(
-        <div> <h1> The Mystical Beverage</h1>
-        <img src={img9} className="photo2" alt=""/></div>
-        )
-    }
-}
+
 
 export default Nav 
+
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+             <h1> The Mystical Beverage</h1>
+        <img src={img9} className="photo2" alt=""/>
+        </div>
+    )
+}
 
 
