44c1117395876d4f9142117060c7a6012c1f6b8a
Vincent Ivos <vincentivos1@gmail.com>
2021-11-04 23:32:46 -0400

    import image origin container

--- src/containers/OriginContainer.js
+++ src/containers/OriginContainer.js
@@ -4,6 +4,15 @@ import { fetchBeans } from '../actions/beansactions'
 import IncrementButton from '../components/IncrementButton'
 import OriginImage from '../components/OriginImage'
 
+import img1 from '../images/fahd-ahmed1.jpg'
+import img2 from '../images/erik-hathaway.jpg'
+import img3 from '../images/bernard-hermant.jpg'
+import img4 from '../images/mohammed-hassan.jpg'
+import img5 from '../images/cris-tagupa.jpg'
+import img6 from '../images/alan-graph.jpg'
+
+impor
+
 
 
  class OriginContainer extends Component {    
