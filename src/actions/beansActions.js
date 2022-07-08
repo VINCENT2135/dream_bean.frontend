408e6b1020f7f20382122c93745c89dd526df3db
Vincent K Ivos <68698502+VINCENT2135@users.noreply.github.com>
2022-07-08 19:35:01 -0400

    Update beansActions.js

--- src/actions/beansActions.js
+++ src/actions/beansActions.js
@@ -28,32 +28,4 @@ export const addBeans = beans => { console.log("Hello There");
     }
 } 
 
-export const fetchVarieties = () => {console.log("Variety");
-return  dispatch => {
-    fetch('http://127.0.0.1:3000/bean_varieties', {
-  
-    headers : { 'Content-Type': 'application/json',
-          'Accept': 'application/json' }
-        }) 
-    .then(resp => resp.json())
-    .then( varieties => dispatch({type: 'FETCH_VARIETIES', payload: varieties})) 
-}
-}
-
-
-
-
-export const addVarieties = varieties => { console.log("Hello There");
-    return dispatch  => {
-        fetch('http://127.0.0.1:3000/bean_varieties', {
-             
-                method: 'POST',
-                body: JSON.stringify(varieties), 
-                headers: { 'Content-Type': 'application/json',
-                'Accept': 'application/json'}
-              
-            })
-        .then(resp => resp.json())
-        .then( beans => dispatch( {type: 'ADD_VARIETIES', payload: varieties}))
-    }
 } 
